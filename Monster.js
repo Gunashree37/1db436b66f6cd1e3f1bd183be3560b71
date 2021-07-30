de608f952096bd89c5ffba4f1a56dab2e8f261cc
Gunashree S <gunashrees341@gmail.com>
2021-07-30 20:36:04 +0530

    Update Monster.js

--- Monster.js
+++ Monster.js
@@ -9,7 +9,9 @@ class Monster {
 		this.x=x;
 		this.y=y;
 		this.r=r;
-		this.image=loadImage("monster.png");
+	
+		
+		
 		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
 		World.add(world, this.body);
 
